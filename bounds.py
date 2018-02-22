9b01f844e6c36a76dbc34f226f0ceb2809875b34
Kunal <kusdhill@ucsc.edu>
2018-02-22 08:20:34 -0800

    boilerplate code, do not plaigerize this

---
+++ bounds.py
@@ -0,0 +1,34 @@
+import PhotoScan, math
+
+doc = PhotoScan.app.document
+chunk = doc.chunk
+region = chunk.region
+T = chunk.transform.matrix
+
+m = PhotoScan.Vector([10E+10, 10E+10, 10E+10])
+M = -m
+
+for point in chunk.point_cloud.points:
+	if not point.valid:
+		continue
+	coord = T * point.coord	
+	coord.size = 3
+	coord = chunk.crs.project(coord)
+	for i in range(3):
+		m[i] = min(m[i], coord[i])
+		M[i] = max(M[i], coord[i])
+		
+center = (M + m) / 2
+size = M - m
+
+region.center = T.inv().mulp(chunk.crs.unproject(center))
+region.size = size * (1 / T.scale())
+
+v_t = T * PhotoScan.Vector( [0,0,0,1] )
+v_t.size = 3
+R = chunk.crs.localframe(v_t) * T
+region.rot = R.rotation().t()
+
+chunk.region = region
+print("Script finished.")
+
