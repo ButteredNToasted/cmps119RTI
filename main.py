1dc05ffe441f0959f7fbbee0d1bbc0a29363dae3
Kunal <kusdhill@ucsc.edu>
2018-02-22 09:19:10 -0800

    added project flow comments

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# This is the project file we are going to send Tom
+# This is the project file we are going to send CHI
 # Only put non-experimentative code in here that we plan on turning in
 
+# select region to mask
+
+# find polar coordinates of selected region
+
+# convert polar coordinates to cartesian coordinates
+
+# calculate size of box
+
+# resize bounding area to be slightly larger than the coordinates
+
+# build the denst cloud
+
+# build the mesh
+
+# save
